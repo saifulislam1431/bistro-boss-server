2d78238726c098aceedf512048a548f66248b332
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-31 00:58:21 +0600

    user,menu and cart APIs created

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors")
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cnv71c.mongodb.net/?retryWrites=true&w=majority`;
 app.use(cors())
 app.use(express.json())
+
+// JWT
 
 
 
@@ -26,6 +29,83 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const menuCollection = client.db("bistroBoss").collection("menu");
+    const cartCollection = client.db("bistroBoss").collection("cart");
+    const usersCollection = client.db("bistroBoss").collection("users");
+
+    // Get Users
+    app.get("/users",async(req,res)=>{
+      const result =await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // User operation
+    app.post("/users" , async(req,res)=>{
+      const body = req.body;
+      const query = {email:body.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message : "User exist"})
+      }
+      const result = await usersCollection.insertOne(body);
+      res.send(result)
+    })
+
+    app.patch("/users/admin/:id", async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const userUpdate = {
+        $set:{
+          role : "admin"
+        }
+      };
+      const result = await usersCollection.updateOne(filter,userUpdate);
+      res.send(result)
+    })
+
+// Get Menu
+    app.get("/allMenus", async (req, res) => {
+      const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+
+    // Get user cart
+    app.get("/carts", async (req,res)=>{
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    
+    app.get("/carts/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    // Cart Operation
+    app.post("/carts", async (req, res) => {
+      const body = req.body;
+      const result = await cartCollection.insertOne(body)
+      res.send(result) 
+    })
+
+    app.delete("/carts/:id" , async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Server successfully connected to MongoDB!");
@@ -41,10 +121,10 @@ run().catch(console.dir);
 
 
 
-app.get("/",(req,res)=>{
-    res.send("Bistro Boss Restaurant server")
+app.get("/", (req, res) => {
+  res.send("Bistro Boss Restaurant server")
 })
 
-app.listen(port,()=>{
-    console.log(`This server listening at port ${port}`);
+app.listen(port, () => {
+  console.log(`This server listening at port ${port}`);
 })
